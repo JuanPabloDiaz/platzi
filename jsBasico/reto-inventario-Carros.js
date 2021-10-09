006ca690a7cef27dce89013cc99e2969fa75914c
Juan Diaz <juanchodis@hotmail.com>
2021-10-09 08:01:22 -0400

    js

--- jsBasico/reto-inventario-Carros.js
+++ jsBasico/reto-inventario-Carros.js
@@ -1,16 +1,22 @@
 //function constructora:
 
 function auto(MARCA, MODELO, COLOR, ANNO) {
-  (this.marca = MARCA),
-    (this.modelo = MODELO),
-    (this.color = COLOR),
-    (this.anno = ANNO);
+  this.marca = MARCA;
+  this.modelo = MODELO;
+  this.color = COLOR;
+  this.anno = ANNO;
 }
+var autos = [];
+
 //generar un ciclo q pregunte 30 veces:
-for (i = 0; i < 30; i++) {
+for (var i = 0; i < 30; i++) {
   var askMarca = prompt(i + " Cual es la marca:");
   var askModelo = prompt(i + " Cual es el modelo:");
   var askColor = prompt(i + " Cual es el color:");
   var askAno = prompt(i + " Cual es el ano:");
   console.log(askAno + askColor + askMarca + askModelo);
+}
+
+for (let i = 0; i < autos.length; i++) {
+  console.log(autos[i]);
 }
