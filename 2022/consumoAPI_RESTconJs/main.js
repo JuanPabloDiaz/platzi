5b6ba427c672097ba79f7b9ec2d5ccbf56c2c371
Juan Diaz <juanchodis@hotmail.com>
2022-06-12 12:25:33 -0400

    api with button

--- 2022/consumoAPI_RESTconJs/main.js
+++ 2022/consumoAPI_RESTconJs/main.js
@@ -2,9 +2,19 @@ console.log('breaking bad api')
 
 const URL = 'https://www.breakingbadapi.com/api/character/random';
 
-fetch(URL)
-  .then(res => res.json())
-  .then(data => {
+// fetch(URL)
+//   .then(res => res.json())
+//   .then(data => {
+//     const img = document.querySelector('img');
+//     img.src = data[0].img;
+//   });
+
+async function random_img () {
+    const res = await fetch(URL);
+    const data = await res.json();
     const img = document.querySelector('img');
     img.src = data[0].img;
-  });
+}
+
+const myButton = document.querySelector("button");
+myButton.onclick = random_img;
