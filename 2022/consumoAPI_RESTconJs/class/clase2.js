d2a1045f7aeaf0a8362336d340666f6e09da5a63
Juan Diaz <juanchodis@hotmail.com>
2022-06-14 08:56:00 -0400

    reload button added

--- 2022/consumoAPI_RESTconJs/class/clase2.js
+++ 2022/consumoAPI_RESTconJs/class/clase2.js
@@ -1,13 +1,14 @@
 // CLASE # 2
 
-console.log('Hello, world')
+console.log("Hello, world");
 
-  const API_URL='https://api.thecatapi.com/v1/images/search';
-  
-  async function reload(){
-  const res=await fetch(API_URL);
-  const data=await res.json();
-  const img=document.querySelector('img');
-  img.src=data[0].url;
+const API_URL = "https://api.thecatapi.com/v1/images/search";
+
+async function reload() {
+  const res = await fetch(API_URL);
+  const data = await res.json();
+
+  const img = document.querySelector("img");
+  img.src = data[0].url;
 }
 reload();
