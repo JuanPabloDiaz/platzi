eb291ed604832d90229cc158c02eea13da7429ac
Juan Diaz <juanchodis@hotmail.com>
2022-06-17 10:56:50 -0400

    GET Favorites

--- 2022/consumoAPI_RESTconJs/class/clase5.js
+++ 2022/consumoAPI_RESTconJs/class/clase5.js
@@ -2,34 +2,38 @@
 
 // Here's my API key for this project:    cff65010-e85e-45f5-a948-45dd27cac97c
 
-console.log("using an API KEY");
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=cff65010-e85e-45f5-a948-45dd27cac97c';
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=cff65010-e85e-45f5-a948-45dd27cac97c';
 
-// Endpoints y query parameters:
-// ?limit=3
+const spanError = document.getElementById('error')
 
-const API_KEY = "cff65010-e85e-45f5-a948-45dd27cac97c";
-
-const API_URL = [
-  "https://api.thecatapi.com/v1/images/search",
-  "?limit=3",
-  `&api_key=${API_KEY}`,
-].join("");
-
-// console.log('API_URL-> ', API_URL);
-// console.log('key -> ', API_KEY);
-
-
-async function reload() {
-  const res = await fetch(API_URL);
+async function loadRandomMichis() {
+  const res = await fetch(API_URL_RANDOM);
   const data = await res.json();
+  console.log('Random')
+  console.log(data)
 
-  console.log(data);
-  const img1 = document.getElementById("img1");
-  const img2 = document.getElementById("img2");
-  const img3 = document.getElementById("img3");
-
-  img1.src = data[0].url;
-  img2.src = data[1].url;
-  img3.src = data[2].url;
+  if (res.status !== 200) {
+    spanError.innerHTML = "Error: " + res.status;
+  } else {
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+  }
 }
-reload();
+
+async function loadFavoritesMichis() {
+  const res = await fetch(API_URL_FAVORITES);
+  const data = await res.json();
+  console.log('Favoritos')
+  console.log(data)
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Error: " + res.status + data.message;
+  }
+}
+
+loadRandomMichis();
+loadFavoritesMichis();
