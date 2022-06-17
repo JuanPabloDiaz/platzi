cad81a9f0d559a47b3c95a4762eeb56b9a3a73e2
Juan Diaz <juanchodis@hotmail.com>
2022-06-17 12:03:40 -0400

    Clase 10: manipulación dinámica del DOM

---
+++ 2022/consumoAPI_RESTconJs/class/clase7-Sirver.js
@@ -0,0 +1,75 @@
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=cff65010-e85e-45f5-a948-45dd27cac97c';
+const API_URL_FAVOTITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=cff65010-e85e-45f5-a948-45dd27cac97c';
+
+const spanError = document.getElementById('error')
+
+async function loadRandomMichis() {
+  const res = await fetch(API_URL_RANDOM);
+  const data = await res.json();
+  console.log('Random')
+  console.log(data)
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status;
+  } else {
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const btn1 = document.getElementById('btn1');
+    const btn2 = document.getElementById('btn2');
+    
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+
+    btn1.onclick = () => saveFavouriteMichi(data[0].id);
+    btn2.onclick = () => saveFavouriteMichi(data[1].id);
+  }
+}
+
+async function loadFavouriteMichis() {
+  const res = await fetch(API_URL_FAVOTITES);
+  const data = await res.json();
+  console.log('Favoritos')
+  console.log(data)
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  } else {
+    data.forEach(michi => {
+      const section = document.getElementById('favoriteMichis')
+      const article = document.createElement('article');
+      const img = document.createElement('img');
+      const btn = document.createElement('button');
+      const btnText = document.createTextNode('Sacar al michi de favoritos');
+
+      img.src = michi.image.url;
+      img.width = 150;
+      btn.appendChild(btnText);
+      article.appendChild(img);
+      article.appendChild(btn);
+      section.appendChild(article);
+    });
+  }
+}
+
+async function saveFavouriteMichi(id) {
+  const res = await fetch(API_URL_FAVOTITES, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      image_id: id
+    }),
+  });
+  const data = await res.json();
+
+  console.log('Save')
+  console.log(res)
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  }
+}
+
+loadRandomMichis();
+loadFavouriteMichis();
