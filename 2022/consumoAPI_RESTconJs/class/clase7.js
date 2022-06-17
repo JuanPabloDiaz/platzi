7324583d2a305ebb44c4dd6b5bde65d631f60648
Juan Diaz <juanchodis@hotmail.com>
2022-06-17 11:50:55 -0400

    clase 10

--- 2022/consumoAPI_RESTconJs/class/clase7.js
+++ 2022/consumoAPI_RESTconJs/class/clase7.js
@@ -1,12 +1,81 @@
-console.log('Hello, world')
+// CLASE # 7 ->  Clase 10: manipulación dinámica del DOM
 
-const URL = 'https://api.thecatapi.com/v1/images/search';
+// Here's my API key for this project:    cff65010-e85e-45f5-a948-45dd27cac97c
 
-fetch(URL)
-  .then(res => res.json())
-  .then(data => {
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+const API_URL_RANDOM =
+  "https://api.thecatapi.com/v1/images/search?limit=2&api_key=cff65010-e85e-45f5-a948-45dd27cac97c";
+const API_URL_FAVORITES =
+  "https://api.thecatapi.com/v1/favourites?limit=2&api_key=cff65010-e85e-45f5-a948-45dd27cac97c";
+
+const spanError = document.getElementById("error");
+
+async function loadRandomMichis() {
+  const res = await fetch(API_URL_RANDOM);
+  const data = await res.json();
+  console.log("Random");
+  console.log(data);
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Error: " + res.status + data.message;
+  } else {
+    const img1 = document.getElementById("img1");
+    const img2 = document.getElementById("img2");
+    const btn1 = document.getElementById("btn1");
+    const btn2 = document.getElementById("btn2");
+
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+
+    btn1.onclick = () => saveFavouriteMichi(data[0].id);
+    btn2.onclick = () => saveFavouriteMichi(data[1].id);
+  }
+}
+
+async function loadFavouriteMichis() {
+  const res = await fetch(API_URL_FAVORITES);
+  const data = await res.json();
+  console.log("Favoritos");
+  console.log(data);
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  } else {
+    data.forEach((michi) => {
+      const section = document.getElementById("favoriteMichis");
+      const article = document.createElement("article");
+      const img = document.createElement("img");
+      const btn = document.createElement("button");
+      const btnText = document.createTextNode("Sacar al michi de favoritos");
+
+      img.src = michi.image.url;
+      img.width = 150;
+      btn.appendChild(btnText);
+      article.appendChild(img);
+      article.appendChild(btn);
+      section.appendChild(article);
+    });
+  }
+}
+
+async function saveFavouriteMichi(id) {
+  const res = await fetch(API_URL_FAVORITES, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      image_id: id,
+    }),
   });
+  const data = await res.json();
 
-  
+  console.log("Save");
+  console.log(res);
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Error: " + res.status + data.message;
+  }
+}
+
+loadRandomMichis();
+loadFavouriteMichis();
