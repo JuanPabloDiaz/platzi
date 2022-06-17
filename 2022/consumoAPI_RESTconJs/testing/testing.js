4851e3a5fc348cd39bce42f3e2d7846703c6c3f3
Juan Diaz <juanchodis@hotmail.com>
2022-06-17 10:57:03 -0400

    testing

--- 2022/consumoAPI_RESTconJs/testing/testing.js
+++ 2022/consumoAPI_RESTconJs/testing/testing.js
@@ -1,12 +1,11 @@
-// CLASE # 1
 
 console.log("this is a testing site");
 
-const URL = "https://rickandmortyapi.com/api/character/161";
+const URL = "https://api.escuelajs.co/api/v1/products/1";
 
 fetch(URL)
   .then(res => res.json())
   .then(data => {
     const img = document.querySelector('img');
-    img.src = data[8].url;
+    img.src = data[0].image;
   });
