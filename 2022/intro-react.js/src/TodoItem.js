c13c25961e82ce25e87f58473d15f0db30b6fe99
Juan Diaz <juanchodis@hotmail.com>
2022-07-09 15:17:04 -0400

    clase 8 - Manejo de eventos

--- 2022/intro-react.js/src/TodoItem.js
+++ 2022/intro-react.js/src/TodoItem.js
@@ -5,7 +5,7 @@ function TodoItem(props) {
   return (
     <li className="TodoItem">
       <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
-        √
+      ✔
       </span>
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
         {props.text}
