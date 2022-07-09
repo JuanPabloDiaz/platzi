a39052a9b636a0802858b77d4e3ebc1d101f4fd1
Juan Diaz <juanchodis@hotmail.com>
2022-07-09 14:03:56 -0400

    clase 6 completada

--- 2022/intro-react.js/src/App.js
+++ 2022/intro-react.js/src/App.js
@@ -5,13 +5,21 @@ import { TodoList } from "./TodoList.js";
 import { TodoItem } from "./TodoItem.js";
 import { CreateTodoButtom } from "./CreateTodoButton.js";
 
+const todos = [
+  {text:'Cortar cebolla', completed:false},
+  {text:'Tormar el curso de intro a react', completed:false},
+  {text:'Llorar con la llorona', completed:false}
+];
+
 function App() {
   return (
    <react.Fragment>
       <TodoCounter />    
       <TodoSearch />
       <TodoList>
-        <TodoItem />
+        {todos.map(todo => (
+            <TodoItem key={todo.text} text={todo.text} />
+        ))}
       </TodoList>
       <CreateTodoButtom />      
    </react.Fragment>
