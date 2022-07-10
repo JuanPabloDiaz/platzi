2d0c414f8b6fb637cc210e27215b165170adf2d0
Juan Diaz <juanchodis@hotmail.com>
2022-07-10 11:49:12 -0400

    clase 12 - Organización de archivos y carpetas

--- 2022/intro-react.js/src/TodoCounter/index.js
+++ 2022/intro-react.js/src/TodoCounter/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './TodoCounter.css';
 
-// Desestructuramos los props que pasamos al componente
 function TodoCounter({ total, completed }) {
   return (
     <h2 className="TodoCounter">Has completado {completed} de {total} TODOs</h2>
   );
 }
 
-export { TodoCounter }; 
+export { TodoCounter };
--- 2022/intro-react.js/src/index.js
+++ 2022/intro-react.js/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import App from './App/index.js';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 );
