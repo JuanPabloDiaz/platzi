6cc1156f6c48a4fe8fbfcde6c64089ad3a334b70
Juan Diaz <juanchodis@hotmail.com>
2022-07-10 09:17:08 -0400

    clase 10 - Contando y buscando TODOs

--- 2022/intro-react.js/src/App.js
+++ 2022/intro-react.js/src/App.js
@@ -1,28 +1,63 @@
-import react from "react";
-import { TodoCounter } from "./TodoCounter";
-import { TodoSearch } from "./TodoSearch.js";
-import { TodoList } from "./TodoList.js";
-import { TodoItem } from "./TodoItem.js";
-import { CreateTodoButton } from "./CreateTodoButton.js";
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+// import './App.css';
 
-const todos = [
-  {text:'Cortar cebolla', completed:false},
-  {text:'Tormar el curso de intro a react', completed:false},
-  {text:'Llorar con la llorona', completed:false}
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar el cursso de intro a React', completed: false },
+  { text: 'Llorar con la llorona', completed: true },
+  { text: 'LALALALAA', completed: false },
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  // El estado de nuestra búsqueda
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+  // Creamos una nueva variable en donde guardaremos las coincidencias con la búsqueda
+  let searchedTodos = [];
+
+  // Lógica para filtrar
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+  
   return (
-   <react.Fragment>
-      <TodoCounter />    
-      <TodoSearch />
+    <React.Fragment>
+      <TodoCounter
+        total={totalTodos}
+        completed={completedTodos}
+      />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+
       <TodoList>
-        {todos.map(todo => (
-            <TodoItem key={todo.text} text={todo.text} />
+        {/* Regresamos solamente los TODOs buscados */}
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
         ))}
       </TodoList>
-      <CreateTodoButton />      
-   </react.Fragment>
+
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
