86ed5912ea1c1a4f94509c4e3b631a384821a883
Juan Diaz <juanchodis@hotmail.com>
2023-09-25 12:50:26 -0400

    pagina actualizada

--- 2023/paginaWebConChatGPT/firstWebsiteChatGPT/script.js
+++ 2023/paginaWebConChatGPT/firstWebsiteChatGPT/script.js
@@ -1,4 +1,40 @@
-// script.js
-window.addEventListener("DOMContentLoaded", (event) => {
-  console.log("La página ha cargado completamente");
+// JavaScript para la landing page
+
+// testing the page
+// window.addEventListener("DOMContentLoaded", (event) => {
+//   console.log("La página ha cargado completamente");
+// });
+
+// barra de navegación interactiva
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll(".section");
+  const navItems = document.querySelectorAll(".nav-item a");
+
+  // Función para resaltar la sección activa en la barra de navegación
+  function setActiveSection() {
+    // const scrollPosition = window.scrollY;
+
+    sections.forEach((section, index) => {
+      const sectionTop = section.offsetTop;
+      const sectionBottom = sectionTop + section.offsetHeight;
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+        navItems[index].classList.add("active-section");
+      } else {
+        navItems[index].classList.remove("active-section");
+      }
+    });
+  }
+
+  // Evento para activar la función cuando se desplace la página
+  window.addEventListener("scroll", setActiveSection);
 });
+
+// Función para manejar el clic en el botón de suscripción
+function suscribirse() {
+  alert("Gracias por suscribirte a nuestra oferta especial.");
+}
+
+// Asociar la función al botón
+const btnSuscribirse = document.getElementById("btnSuscribirse");
+btnSuscribirse.addEventListener("click", suscribirse);
