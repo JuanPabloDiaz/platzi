c4d1c1732a66ce45bb9ec3fa5f795e0ee4cc4d58
Juan Diaz <juanchodis@hotmail.com>
2023-09-25 21:56:04 -0400

    test

--- 2023/copilot/testing.js
+++ 2023/copilot/testing.js
@@ -1,1 +1,5 @@
-function calculateDaysBetweenDates(begin, end) {}
+function calculateDaysBetweenDates(begin, end) {
+  const oneDay = 1000 * 60 * 60 * 24;
+  const differenceMs = Math.abs(end - begin);
+  return Math.round(differenceMs / oneDay);
+}
